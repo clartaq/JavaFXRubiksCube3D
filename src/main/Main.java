9c3e3202756816b956d02ea19422c42836c7b818
clartaq <clartaq@axaluna.com>
2021-01-24 13:38:40 -0500

    Fixed spelling.

--- src/main/Main.java
+++ src/main/Main.java
@@ -64,7 +64,7 @@ public class Main extends Application {
         scene.setCamera(camera);
 
         PhongMaterial mat = new PhongMaterial();
-        String imageName = "resources/pallete.png";
+        String imageName = "resources/palette.png";
         mat.setDiffuseMap(new Image(getClass().getResourceAsStream(imageName)));
 
         Group meshGroup = new Group();
